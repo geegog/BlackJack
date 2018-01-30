310539c1305f6b8854a25f83236e9334aa5a1db1
Onuche Idoko <onuche.idoko@venturegardengroup.com>
2018-01-30 15:44:53 +0100

    More Work on BlackJack

--- twentyone.py
+++ twentyone.py
@@ -4,55 +4,136 @@ from random import *
 
 class Utility(object):
 
-    def __init__(self, cards=None):
-        if cards is None:
-            self.cards = []
-        else:
-            self.cards = cards
+    def __init__(self):
+        print('Utility Constructor')
 
-    def dispense(self):
-        x = sample(self.cards, 1)
+    @staticmethod
+    def picks_count(value):
+        return Deck.picks.count(value)
+
+    @staticmethod
+    def dispense(cards=[]):
+        i = randint(0, 24)
+        shuffle(cards)
+        v = cards[i]
+
         k = None
-        for k in x[0].keys():
+        for k in v.keys():
             k = k
 
         if k == 'FV':
-            r = sample(x[0].get(k), 1)
-            return r[0]
-        elif k == 'A':
-            return x[0].get(k)
+            r = sample(v.get(k), 1)
+            return k+str(r[0]), r[0]
         else:
-            return x[0].get(k)
+            return k, v.get(k)
+
+    @staticmethod
+    def compute(hand=[]):
+        total = 0
+        for i in hand:
+            try:
+                total += i
+            except TypeError:
+                key, value = i
+                low, high = value
+                if (total + low) < 21:
+                    total += high
+                elif (total + high) > 21:
+                    total += low
+        return total
+
+
+class Hand(object):
+
+    def __int__(self, c=None):
+        self.c = c
+
+    def get_hand(self):
+        return self.c
+
+    def add_hand(self, value):
+        return self.c.append(value)
 
 
 class Deck(object):
 
     cards = [{"FV": range(2, 11)}, {"K": 10}, {"Q": 10}, {"A": (1, 11)}, {"J": 10}] * 5
-    picks = {}
+    picks = []
 
     def __init__(self):
-        print('Empty Constructor')
+        print('Deck Constructor')
+
+    def bet(self):
+        value, result = Utility.dispense(self.cards)
+
+        if Utility.picks_count(value) > 5:
+            Deck.bet(self)
+
+            Deck.picks = value
+        if value == 'A':
+            return value, result
+        return result
+
+    def init_hand(self):
+
+        i_h = []
+        i = 0
+        while True:
+            value, result = Utility.dispense(self.cards)
+            Deck.picks = value
+
+            if value == 'A':
+                i_h.append((value, result))
+            else:
+                i_h.append(result)
+
+            i += 1
+            if i == 2:
+                break
+
+        return i_h
 
 
 class Player(object):
 
-    cash = 50
-    hand = []
-
-    def __init__(self, cash):
+    def __init__(self, cash, bet_amount=50, hand=None):
+        if hand is None:
+            hand = []
         self.cash = cash
+        self.bet_amount = bet_amount
+        self.hand = hand
 
     def bet(self, amount):
+        self.bet_amount = amount
         self.cash -= amount
+
+    def win(self):
+        self.cash = self.bet_amount * 2
+
+    def loss(self):
+        self.cash = self.cash
+
+    def hit(self, dk=Deck().bet()):
+        self.hand.append(dk)
+        return self.hand
 
 
 class Dealer(object):
 
-    hand = []
-
-    def __init__(self, hand):
+    def __init__(self, hand=None):
+        if hand is None:
+            hand = []
         self.hand = hand
 
 
-y = Utility(Deck().cards)
-print(y.dispense())
+deck = Deck()
+player = Player(500, 250, deck.init_hand())
+dealer = Dealer(deck.init_hand())
+
+player.hit(deck.bet())
+
+print(Utility.compute(player.hand))
+
+print(player.hand)
+print(player.cash)
+print(player.bet_amount)
