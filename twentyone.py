92163835db6254dbc497ac3dc1229e802594c591
Onuche Idoko <onuche.idoko@venturegardengroup.com>
2018-02-02 12:46:27 +0100

    BlackJack Play Logic Added

--- twentyone.py
+++ twentyone.py
@@ -12,7 +12,9 @@ class Utility(object):
         return Deck.picks.count(value)
 
     @staticmethod
-    def dispense(cards=[]):
+    def dispense(cards=None):
+        if cards is None:
+            cards = []
         i = randint(0, 24)
         shuffle(cards)
         v = cards[i]
@@ -28,18 +30,33 @@ class Utility(object):
             return k, v.get(k)
 
     @staticmethod
-    def compute(hand=[]):
+    def compute(hand=None):
+        if hand is None:
+            hand = []
+
+        aces = hand.count('A')
+        low = 1
+        high = 11
+
+        if (aces == 1) and (Utility.count_hand_without_aces(hand) + high > 21):
+            return Utility.count_hand_without_aces(hand) + low
+        elif (aces == 1) and (Utility.count_hand_without_aces(hand) + high == 21):
+            return Utility.count_hand_without_aces(hand) + high
+        elif (aces == 2) and (Utility.count_hand_without_aces(hand) + high + low == 21):
+            return Utility.count_hand_without_aces(hand) + high + low
+        elif (aces == 2) and (Utility.count_hand_without_aces(hand) + high + low > 21):
+            return Utility.count_hand_without_aces(hand) + low + low
+        else:
+            return Utility.count_hand_without_aces(hand)
+
+    @staticmethod
+    def count_hand_without_aces(hand=None):
         total = 0
         for i in hand:
             try:
                 total += i
             except TypeError:
-                key, value = i
-                low, high = value
-                if (total + low) < 21:
-                    total += high
-                elif (total + high) > 21:
-                    total += low
+                pass
         return total
 
 
@@ -71,7 +88,7 @@ class Deck(object):
 
             Deck.picks = value
         if value == 'A':
-            return value, result
+            return value
         return result
 
     def init_hand(self):
@@ -83,7 +100,7 @@ class Deck(object):
             Deck.picks = value
 
             if value == 'A':
-                i_h.append((value, result))
+                i_h.append(value)
             else:
                 i_h.append(result)
 
@@ -96,22 +113,32 @@ class Deck(object):
 
 class Player(object):
 
-    def __init__(self, cash, bet_amount=50, hand=None):
+    def __init__(self, cash, bet_amount=50, hand=None, deck = Deck()):
         if hand is None:
             hand = []
         self.cash = cash
         self.bet_amount = bet_amount
         self.hand = hand
+        self.deck = deck
 
-    def bet(self, amount):
+    def set_hand(self):
+        self.hand = self.deck.init_hand()
+
+    def add_to_hand(self):
+        self.hand.append(self.deck.bet())
+
+    def deal(self, amount):
         self.bet_amount = amount
         self.cash -= amount
 
+    def push(self):
+        self.cash += self.bet_amount
+
     def win(self):
-        self.cash = self.bet_amount * 2
+        self.cash += (self.bet_amount * 3)
 
     def loss(self):
-        self.cash = self.cash
+        return self.cash
 
     def hit(self, dk=Deck().bet()):
         self.hand.append(dk)
@@ -120,20 +147,58 @@ class Player(object):
 
 class Dealer(object):
 
-    def __init__(self, hand=None):
+    def __init__(self, hand=None, deck = Deck()):
         if hand is None:
             hand = []
         self.hand = hand
+        self.deck = deck
+
+    def set_hand(self):
+        self.hand = self.deck.init_hand()
+
+    def add_to_hand(self):
+        self.hand.append(self.deck.bet())
 
 
-deck = Deck()
-player = Player(500, 250, deck.init_hand())
-dealer = Dealer(deck.init_hand())
+def play():
+    deck = Deck()
+    player = Player(500, deck)
+    dealer = Dealer(deck)
 
-player.hit(deck.bet())
+    amount = int(raw_input('Enter Betting Amount'))
 
-print(Utility.compute(player.hand))
+    player.deal(amount)
+    player.set_hand()
+    dealer.set_hand()
 
-print(player.hand)
-print(player.cash)
-print(player.bet_amount)
+    print('Player Hand', player.hand)
+    print('Dealer Hand', dealer.hand)
+
+    player_action = int(raw_input('Enter 1 for a HIT and 2 for a STAND'))
+
+    while player_action == 1:
+        player.add_to_hand()
+        player_action = int(raw_input('Enter 1 for a HIT and 2 for a STAND'))
+
+    if player_action == 2:
+        if Utility.compute(dealer.hand) <= 17:
+            dealer.add_to_hand()
+
+        if Utility.compute(player.hand) == 21 and Utility.compute(dealer.hand) != 21:
+            player.win()
+            print('BlackJack', player.hand, player.cash)
+        elif Utility.compute(player.hand) == 21 and Utility.compute(dealer.hand) == 21:
+            player.push()
+            print('Push', player.hand, player.cash)
+        elif Utility.compute(player.hand) == Utility.compute(dealer.hand):
+            player.push()
+            print('Push', player.hand, player.cash)
+        elif Utility.compute(player.hand) > Utility.compute(dealer.hand):
+            player.win()
+            print('Win', player.hand, player.cash)
+        elif Utility.compute(player.hand) < Utility.compute(dealer.hand):
+            player.loss()
+            print('Loss', player.hand, player.cash)
+
+
+play()
